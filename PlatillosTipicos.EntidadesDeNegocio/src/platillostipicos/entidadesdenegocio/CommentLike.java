7ab46fee7aed783e81518e1acc48d77e052fa9e5
hbonilla6 <hbonilla1946@gmail.com>
2023-05-28 23:03:11 -0600

    Entidad de CommentLike y PublicationLike

--- PlatillosTipicos.EntidadesDeNegocio/src/platillostipicos/entidadesdenegocio/CommentLike.java
+++ PlatillosTipicos.EntidadesDeNegocio/src/platillostipicos/entidadesdenegocio/CommentLike.java
@@ -1,13 +1,92 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package platillostipicos.entidadesdenegocio;
 
-/**
- *
- * @author edefl
- */
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class CommentLike {
-    
+
+    private int top_aux;
+    private UUID Id;
+    private boolean IsLike;
+    private LocalDateTime CreateDate;
+    private UUID UserId;
+    private UUID CommentId;
+    private User User;
+    private Comment Comment;
+
+    public CommentLike() {
+    }
+
+    public CommentLike(UUID Id, boolean IsLike, LocalDateTime CreateDate, UUID UserId, UUID CommentId) {
+        this.Id = Id;
+        this.IsLike = IsLike;
+        this.CreateDate = CreateDate;
+        this.UserId = UserId;
+        this.CommentId = CommentId;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+
+    public UUID getId() {
+        return Id;
+    }
+
+    public void setId(UUID Id) {
+        this.Id = Id;
+    }
+
+    public boolean isIsLike() {
+        return IsLike;
+    }
+
+    public void setIsLike(boolean IsLike) {
+        this.IsLike = IsLike;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return CreateDate;
+    }
+
+    public void setCreateDate(LocalDateTime CreateDate) {
+        this.CreateDate = CreateDate;
+    }
+
+    public UUID getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(UUID UserId) {
+        this.UserId = UserId;
+    }
+
+    public UUID getCommentId() {
+        return CommentId;
+    }
+
+    public void setCommentId(UUID CommentId) {
+        this.CommentId = CommentId;
+    }
+
+    public User getUser() {
+        return User;
+    }
+
+    public void setUser(User User) {
+        this.User = User;
+    }
+
+    public Comment getComment() {
+        return Comment;
+    }
+
+    public void setComment(Comment Comment) {
+        this.Comment = Comment;
+    }
+
 }
--- PlatillosTipicos.EntidadesDeNegocio/src/platillostipicos/entidadesdenegocio/PublicationLike.java
+++ PlatillosTipicos.EntidadesDeNegocio/src/platillostipicos/entidadesdenegocio/PublicationLike.java
@@ -1,13 +1,92 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package platillostipicos.entidadesdenegocio;
 
-/**
- *
- * @author edefl
- */
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class PublicationLike {
-    
+
+    private int top_aux;
+    private UUID Id;
+    private boolean IsLike;
+    private LocalDateTime CreateDate;
+    private UUID UserId;
+    private UUID PublicationId;
+    private User User;
+    private Publication Publication;
+
+    public PublicationLike() {
+    }
+
+    public PublicationLike(UUID Id, boolean IsLike, LocalDateTime CreateDate, UUID UserId, UUID PublicationId) {
+        this.Id = Id;
+        this.IsLike = IsLike;
+        this.CreateDate = CreateDate;
+        this.UserId = UserId;
+        this.PublicationId = PublicationId;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+
+    public UUID getId() {
+        return Id;
+    }
+
+    public void setId(UUID Id) {
+        this.Id = Id;
+    }
+
+    public boolean isIsLike() {
+        return IsLike;
+    }
+
+    public void setIsLike(boolean IsLike) {
+        this.IsLike = IsLike;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return CreateDate;
+    }
+
+    public void setCreateDate(LocalDateTime CreateDate) {
+        this.CreateDate = CreateDate;
+    }
+
+    public UUID getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(UUID UserId) {
+        this.UserId = UserId;
+    }
+
+    public UUID getPublicationId() {
+        return PublicationId;
+    }
+
+    public void setPublicationId(UUID PublicationId) {
+        this.PublicationId = PublicationId;
+    }
+
+    public User getUser() {
+        return User;
+    }
+
+    public void setUser(User User) {
+        this.User = User;
+    }
+
+    public Publication getPublication() {
+        return Publication;
+    }
+
+    public void setPublication(Publication Publication) {
+        this.Publication = Publication;
+    }
+
 }
