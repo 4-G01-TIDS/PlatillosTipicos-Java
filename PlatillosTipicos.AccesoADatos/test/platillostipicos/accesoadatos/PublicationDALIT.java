0072b71daeba786201616251f6c7bdf4ec281581
hbonilla6 <hbonilla1946@gmail.com>
2023-06-10 15:10:25 -0600

    Prueba unitaria de Publication: PublicationDALIT

--- PlatillosTipicos.AccesoADatos/test/platillostipicos/accesoadatos/PublicationDALIT.java
+++ PlatillosTipicos.AccesoADatos/test/platillostipicos/accesoadatos/PublicationDALIT.java
@@ -1,21 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
 package platillostipicos.accesoadatos;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import java.sql.ResultSet;
+import java.time.LocalDateTime;
+import java.util.*;
+import org.junit.*;
+import java.sql.*;
 import static org.junit.Assert.*;
+import org.junit.runners.MethodSorters;
+import platillostipicos.entidadesdenegocio.Publication;
 
-/**
- *
- * @author edefl
- */
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PublicationDALIT {
+
+    private static Publication currentPublication;
 
     public PublicationDALIT() {
     }
@@ -36,12 +34,147 @@ public class PublicationDALIT {
     public void tearDown() {
     }
 
+    /**
+     * Test of crear method, of class PublicationDAL.
+     */
     @Test
-    public void testObtenerCampos() {
-        System.out.println("obtenerCampos");
-        String expResult = "u.Id, u.Description, u.PublicationDate, u.UserId, u.PublicationImagesId, u.RestaurantId, u.User";
-        String result = PublicationDAL.obtenerCampos();
+    public void test1Crear() throws Exception {
+        System.out.println("crear");
+        Publication pPublication = new Publication();
+        pPublication.setDescription("Description UNIT TEST");
+        pPublication.setPublicationDate(LocalDateTime.MIN);
+        pPublication.setUserId(UUID.fromString("23366d79-f322-4448-9e55-1518fc0c34b0"));
+        pPublication.setRestaurantId(UUID.fromString("807487cc-fc4d-11ed-be56-0242ac120002"));
+        int expResult = 1;
+        int result = PublicationDAL.crear(pPublication);
         assertEquals(expResult, result);
     }
 
+    public int testIndividualQuerySelect(Publication pPublication) throws Exception {
+        ComunDB comundb = new ComunDB();
+        ComunDB.UtilQuery pUtilQuery = comundb.new UtilQuery("", null, 0);
+        PublicationDAL.querySelect(pPublication, pUtilQuery);
+        return pUtilQuery.getNumWhere();
+    }
+
+    /**
+     * Test of GetById method, of class PublicationDAL.
+     */
+    @Test
+    public void test4GetById() throws Exception {
+        System.out.println("GetById");
+        Publication result = PublicationDAL.GetById(currentPublication);
+        assertEquals(currentPublication.getId(), result.getId());
+    }
+
+    /**
+     * Test of obtenerCampos method, of class PublicationDAL.
+     */
+    @Test
+    public void test7ObtenerCampos() {
+        System.out.println("obtenerCampos");
+        String expResult = "";
+        String result = PublicationDAL.obtenerCampos();
+        assertNotEquals(expResult, result);
+    }
+    /**
+     * Test of asignarDatosResultSet method, of class PublicationDAL.
+     */
+    @Test
+    public void test6AsignarDatosResultSet() throws Exception {
+        System.out.println("asignarDatosResultSet");
+        Publication publication = new Publication();
+        try (Connection conn = ComunDB.obtenerConexion();) {
+            String sql = "SELECT " + PublicationDAL.obtenerCampos() + " FROM Publications u";
+            sql += " WHERE u.Id=?";
+            try (PreparedStatement ps = ComunDB.createPreparedStatement(conn, sql);) {
+                ps.setString(1, currentPublication.getId().toString());
+                try (ResultSet resultSet = ComunDB.obtenerResultSet(ps);) {
+                    while (resultSet.next()) {
+                        PublicationDAL.asignarDatosResultSet(publication, resultSet, 0);
+                    }
+                    resultSet.close();
+                } catch (SQLException ex) {
+                    throw ex;
+                }
+                ps.close();
+            } catch (SQLException ex) {
+                throw ex;
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            throw ex;
+        }
+        System.out.println(publication.getId().getClass() + " " + publication.getId());
+        System.out.println(currentPublication.getId().getClass() + " " + currentPublication.getId());
+        assertTrue(publication.getId().equals(currentPublication.getId()));
+    }
+
+    /**
+     * Test of getPublications method, of classz PublicationDAL.
+     */
+    @Test
+    public void test3GetPublications() throws Exception {
+        System.out.println("getPublications");
+        Publication pPublication = new Publication();
+        pPublication.setDescription("Description UNIT TEST");
+        pPublication.setUserId(UUID.fromString("23366d79-f322-4448-9e55-1518fc0c34b0"));
+        pPublication.setRestaurantId(UUID.fromString("807487cc-fc4d-11ed-be56-0242ac120002"));
+        ArrayList<Publication> result = PublicationDAL.getPublications(pPublication);
+        assertTrue(result.size() > 0);
+        currentPublication = result.get(0);
+    }
+
+    /**
+     * Test of querySelect method, of class PublicationDAL.
+     */
+    @Test
+    public void test2QuerySelect() throws Exception {
+        System.out.println("querySelect");
+        int index = 0;
+        Publication pPublication = new Publication();
+        pPublication.setId(UUID.randomUUID());
+        index++;
+        assertTrue(testIndividualQuerySelect(pPublication) == index);
+        pPublication.setDescription("TEST");
+        index++;
+        assertTrue(testIndividualQuerySelect(pPublication) == index);
+        pPublication.setPublicationDate(LocalDateTime.MIN);
+        index++;
+        assertTrue(testIndividualQuerySelect(pPublication) == index);
+        pPublication.setUserId(UUID.randomUUID());
+        index++;
+        assertTrue(testIndividualQuerySelect(pPublication) == index);
+        pPublication.setPublicationImagesId(UUID.randomUUID());
+        index++;
+        assertTrue(testIndividualQuerySelect(pPublication) == index);
+        pPublication.setRestaurantId(UUID.randomUUID());
+    }
+
+    /**
+     * Test of modificar method, of class PublicationDAL.
+     */
+    @Test
+    public void test5Modificar() throws Exception {
+        System.out.println("modificar");
+        Publication pPublication = new Publication();
+        pPublication.setId(currentPublication.getId());
+        pPublication.setDescription("Description UNIT TEST M");
+
+        int expResult = 1;
+        int result = PublicationDAL.modificar(pPublication);
+        assertEquals(expResult, result);
+    }
+    /**
+     * Test of eliminar method, of class PublicationDAL.
+     */
+      @Test
+    public void test8Eliminar() throws Exception {
+        System.out.println("eliminar");
+        int expResult = 1;
+        int result = PublicationDAL.eliminar(currentPublication);
+        assertEquals(expResult, result);
+        Publication deletePublication = PublicationDAL.GetById(currentPublication);
+          assertTrue(deletePublication.getId() == null);
+    }
 }
